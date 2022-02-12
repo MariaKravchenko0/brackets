1bee51137a7719878fa73ed8e7113a833aaa97fb
MariaKravchenko0 <kravchenkomaria00@gmail.com>
2022-02-12 22:54:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let bracketsConfigObj = Object.fromEntries(bracketsConfig);
+  let openingBrackets = Object.keys(bracketsConfigObj);
+  let sameBrackets = [];
+
+  bracketsConfig.forEach((bracketsPair) => {
+    if (bracketsPair[0] === bracketsPair[1]) {
+      sameBrackets.push(bracketsPair[0]);
+    }
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topSymbol = stack[stack.length - 1];
+
+    if (sameBrackets.includes(currentSymbol)) {
+      if (currentSymbol === topSymbol) {
+        stack.pop();
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else if (openingBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else if (bracketsConfigObj[topSymbol] === currentSymbol) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
